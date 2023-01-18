a3aa1a8bb228e1e504cebc65a85e568570e18d91
yasuomaidana <yasuomaidana@gmail.com>
2023-01-18 14:26:26 +0900

    Testing variables in react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
+
+  const name = "Yasuo"
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. My name is: {name}
         </p>
         <a
           className="App-link"
