04eb6f74179900370998877ebf6ffd6970f90502
yasuomaidana <yasuomaidana@gmail.com>
2023-01-21 00:39:11 +0900

    Moving header, content and footer to components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
-import logo from './logo.svg';
+import Header from './Header';
 import './App.scss';
+import Content from './Content';
+import Footer from './Footer';
 
 function App() {
-
-  const handleNameChange = () => {
-    const names = ["Yasuo","Nadia", "Bejamin"];
-    const int = Math.floor(Math.random()*3);
-    return names[int];
-  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. My name is: {handleNameChange()}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Content/>
+      <Footer/>
     </div>
   );
 }
