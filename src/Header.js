9f0f0b86b3638b55225c144f040f4644a58af99d
yasuomaidana <yasuomaidana@gmail.com>
2023-01-21 00:47:43 +0900

    Adding style by using object

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
 export default function Header() {
+  const headerStyle = {
+    backgroundColor: 'mediumblue',
+    color:'#fff',
+  };
   return (
-    <header>
+    <header style={headerStyle}>
         <h1>Groceries list</h1>
     </header>
   )
