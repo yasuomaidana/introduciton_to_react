6dfbedde65041ec937783e9dac2b25f9320b8d47
yasuomaidana <yasuomaidana@gmail.com>
2023-01-23 02:18:28 +0900

    Adding logic through buttons

--- src/Content.js
+++ src/Content.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React from "react";
 
 export default function Content() {
-        const handleName = () =>{
-            const names = ["Yasuo","Nadia","Benjamin"];
-            const int = Math.floor(Math.random()*3);
-            return names[int];
-        }
+  const handleName = () => {
+    const names = ["Yasuo", "Nadia", "Benjamin"];
+    const int = Math.floor(Math.random() * 3);
+    return names[int];
+  };
+  const handleClick = () =>{
+    console.log("Hi, you clicked me");
+  }
+  const handleClick2 = (name) =>{
+    console.log(`Hi, ${name}`);
+  }
+  const handleClick3 = (e) =>{
+    console.log(e.target.innerText);
+  }
   return (
     <main>
-        {handleName()}
+      <p>{handleName()}</p>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={() =>handleClick2(handleName())}>Click me</button>
+      <button onClick={(e) =>handleClick3(e)}>Click me</button>
     </main>
-  )
+  );
 }
