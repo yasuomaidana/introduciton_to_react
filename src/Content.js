231e24ee2941bf8e1d881af460dffdf6861c2b19
yasuomaidana <yasuomaidana@gmail.com>
2023-01-24 01:57:25 +0900

    Changing state values using previous ones

--- src/Content.js
+++ src/Content.js
@@ -1,26 +1,26 @@
-import React from "react";
+import {useState} from "react";
 
 export default function Content() {
+  const [name, setName] =  useState('Yasuo')
+  const [count, setCount] =  useState(0)
   const handleName = () => {
     const names = ["Yasuo", "Nadia", "Benjamin"];
     const int = Math.floor(Math.random() * 3);
     return names[int];
   };
-  const handleClick = () =>{
-    console.log("Hi, you clicked me");
+  const handleChangeNAme = () =>{
+    setName(handleName())
   }
-  const handleClick2 = (name) =>{
-    console.log(`Hi, ${name}`);
-  }
-  const handleClick3 = (e) =>{
-    console.log(e.target.innerText);
+  const handleIncreaseCount = () =>{
+    var currentCount = count;
+    currentCount+=1;
+    setCount(currentCount);
   }
   return (
     <main>
-      <p>{handleName()}</p>
-      <button onClick={handleClick}>Click me</button>
-      <button onClick={() =>handleClick2(handleName())}>Click me</button>
-      <button onClick={(e) =>handleClick3(e)}>Click me</button>
+      <p onDoubleClick={handleChangeNAme}>Hi {name}! <br/>{count}</p>
+      <button onClick={handleChangeNAme}>Click me to change name</button>
+      <button onClick={handleIncreaseCount}>Click me to increase count</button>
     </main>
   );
 }
