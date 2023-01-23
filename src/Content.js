9e419a4a898d75321b19f8a99659c6152e330474
yasuomaidana <yasuomaidana@gmail.com>
2023-01-24 02:09:16 +0900

    Preparing to implement delete item from list

--- src/Content.js
+++ src/Content.js
@@ -1,26 +1,34 @@
 import {useState} from "react";
 
 export default function Content() {
-  const [name, setName] =  useState('Yasuo')
-  const [count, setCount] =  useState(0)
-  const handleName = () => {
-    const names = ["Yasuo", "Nadia", "Benjamin"];
-    const int = Math.floor(Math.random() * 3);
-    return names[int];
-  };
-  const handleChangeNAme = () =>{
-    setName(handleName())
-  }
-  const handleIncreaseCount = () =>{
-    var currentCount = count;
-    currentCount+=1;
-    setCount(currentCount);
-  }
+  const [items, setItems] =  useState([
+    {
+      id:1,
+      checked: false,
+      item: "Item 1"
+    },
+    {
+      id:2,
+      checked: true,
+      item: "Item 2"
+    },
+    {
+      id:3,
+      checked: false,
+      item: "Item 3"
+    }
+  ]);
+  
   return (
     <main>
-      <p onDoubleClick={handleChangeNAme}>Hi {name}! <br/>{count}</p>
-      <button onClick={handleChangeNAme}>Click me to change name</button>
-      <button onClick={handleIncreaseCount}>Click me to increase count</button>
+      <ul>
+        {items.map(item=> (
+        <li className={"item"+item.id} key={item.id}>
+          <input type="checkbox" checked={item.checked}/>
+          <label>{item.item}</label>
+          <button>Delete</button>
+        </li>) )}
+      </ul>
     </main>
   );
 }
