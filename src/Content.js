1b442d1b91bd81bc703aeb7a7cddde97245c4e90
yasuomaidana <yasuomaidana@gmail.com>
2023-01-25 19:29:04 +0900

    Handling delete button

--- src/Content.js
+++ src/Content.js
@@ -1,33 +1,46 @@
-import {useState} from "react";
+import { useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
 
 export default function Content() {
-  const [items, setItems] =  useState([
+  const [items, setItems] = useState([
     {
-      id:1,
-      checked: false,
-      item: "Item 1"
-    },
-    {
-      id:2,
+      id: 1,
       checked: true,
-      item: "Item 2"
+      item: "Item 1",
     },
     {
-      id:3,
+      id: 2,
       checked: false,
-      item: "Item 3"
-    }
+      item: "Item 2",
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "Item 3",
+    },
   ]);
+
+  const handleCheck = (id) => {
+    const listItems = items.map( item => item.id === id ? { ...item, checked: !item.checked} : item);
+    setItems(listItems);
+    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+  }
   
+  const handleDelete = (id) =>{
+    const listItems = items.filter(item => item.id !== id)
+    setItems(listItems)
+    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+  }
   return (
     <main>
       <ul>
-        {items.map(item=> (
-        <li className={"item"+item.id} key={item.id}>
-          <input type="checkbox" checked={item.checked}/>
-          <label>{item.item}</label>
-          <button>Delete</button>
-        </li>) )}
+        {items.map((item) => (
+          <li className="item" key={item.id}>
+            <input type="checkbox" checked={item.checked} onChange={() => handleCheck(item.id)} />
+            <label>{item.item}</label>
+            <FaTrashAlt onClick={() => handleDelete(item.id)} role="button" tabIndex="0"/>
+          </li>
+        ))}
       </ul>
     </main>
   );
