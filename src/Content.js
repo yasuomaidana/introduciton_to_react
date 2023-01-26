b88b83d3261d0d0d63348cda5d1fdc1cab6fcce2
yasuomaidana <yasuomaidana@gmail.com>
2023-01-27 02:21:45 +0900

    Sharing information between two modules

--- src/Content.js
+++ src/Content.js
@@ -1,36 +1,7 @@
-import { useState } from "react";
+
 import { FaTrashAlt } from "react-icons/fa";
 
-export default function Content() {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: true,
-      item: "Item 1",
-    },
-    {
-      id: 2,
-      checked: false,
-      item: "Item 2",
-    },
-    {
-      id: 3,
-      checked: false,
-      item: "Item 3",
-    },
-  ]);
-
-  const handleCheck = (id) => {
-    const listItems = items.map( item => item.id === id ? { ...item, checked: !item.checked} : item);
-    setItems(listItems);
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
-  }
-  
-  const handleDelete = (id) =>{
-    const listItems = items.filter(item => item.id !== id)
-    setItems(listItems)
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
-  }
+export default function Content({items, handleCheck, handleDelete}) {
 
   const drawList = () => {
     return (
