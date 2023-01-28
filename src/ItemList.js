c9d84a81b0d1e8efc3d5af9a03fc5ffa305c6af1
yasuomaidana <yasuomaidana@gmail.com>
2023-01-29 02:30:09 +0900

    Creating ItemList component

---
+++ src/ItemList.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { FaTrashAlt } from "react-icons/fa";
+const ItemList = ({items, handleCheck, handleDelete}) => {
+  return (
+    <ul>
+        {items.map((item) => (
+          <li className="item" key={item.id}>
+            <input
+              type="checkbox"
+              checked={item.checked}
+              onChange={() => handleCheck(item.id)}
+            />
+            <label
+              style={item.checked ? { textDecoration: "line-through" } : null}
+              onDoubleClick={() => handleCheck(item.id)}
+            >
+              {item.item}
+            </label>
+            <FaTrashAlt
+              onClick={() => handleDelete(item.id)}
+              role="button"
+              tabIndex="0"
+            />
+          </li>
+        ))}
+      </ul>
+  )
+}
+
+export default ItemList
