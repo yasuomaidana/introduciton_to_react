d911765a23da6bcaac58836eadbec363d5c60b84
yasuomaidana <yasuomaidana@gmail.com>
2023-02-03 01:27:05 +0900

    Using focus() function

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { FaPlus } from 'react-icons/fa'
+import { useRef } from 'react'
 
 const AddItem = ({newItem, setNewItem, handleSubmit}) => {
+  const inputRef = useRef();
   return (
   <form className='addForm' onSubmit={handleSubmit}>
     <label htmlFor='addItem'>Add item</label>
-    <input autoFocus id='addItem' type='text' placeholder='Add item' required value={newItem}
+    <input autoFocus ref={inputRef} id='addItem' type='text' placeholder='Add item' required value={newItem}
     onChange={(e) => setNewItem(e.target.value)}/>
-    <button type='submit' aria-label='Add item'><FaPlus/></button>
+    <button type='submit' aria-label='Add item' onClick={() => inputRef.current.focus()}><FaPlus/></button>
   </form>
   )
 }
