5765d30d1f04d04568006b542fb60daf0436303c
yasuomaidana <yasuomaidana@gmail.com>
2023-03-01 17:28:25 +0900

    Changing main to fract

--- src/Content.js
+++ src/Content.js
@@ -2,8 +2,8 @@ import ItemList from "./ItemList";
 
 export default function Content({items, handleCheck, handleDelete}) {
   return (
-    <main>
+    <>
       {items.length ? <ItemList items={items} handleCheck={handleCheck} handleDelete={handleDelete}/>:<p style={{marginTop:'2rem',textAlign:"center"}}>Ups your list is Empty!!! <br/> Like your soul</p>}
-    </main>
+    </>
   );
 }
