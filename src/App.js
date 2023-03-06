aba0ed88b67fc402b26aa5889c1375cfefdf7d1f
yasuomaidana <yasuomaidana@gmail.com>
2023-03-06 21:20:05 +0900

    Creating update request

--- src/App.js
+++ src/App.js
@@ -61,9 +61,20 @@ function App() {
     setNewItem('');
   }
 
-  const handleCheck = (id) => {
+  const handleCheck = async (id) => {
     const listItems = items.map( item => item.id === id ? { ...item, checked: !item.checked} : item);
     setItems(listItems);
+    const myItem = listItems.find((item) => item.id === id);
+    const updateOptions = {
+      method: 'PATCH',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body: JSON.stringify({checked: myItem.checked})
+    };
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, updateOptions);
+    if (result) setFetchError(result);
   }
   
   const handleDelete = (id) =>{
