db00a82db59d44cd5073905edc59a39be7a0e745
yasuomaidana <yasuomaidana@gmail.com>
2023-04-24 22:35:00 +0900

    COUR-711: Implementing routes with childrends

--- src/Router.js
+++ src/Router.js
@@ -9,13 +9,26 @@ import App from './App';
 import { Color } from './Color';
 import FetchData from './fetch_data_component/FetchData';
 import Blog from './blog/Blog';
+import Home from "./blog/Home"
+import Post from "./blog/post/Post"
+import PostPage from "./blog/post/PostPage"
 
 const browserRouter = createBrowserRouter(createRoutesFromElements(
 
-        <Route path="/" element={<App />}>
-            <Route path="/color" element={<Color />}/>
+        <Route>
+            <Route path="/" element={<App />}/>
+            <Route path="color">
+              <Route path="" element={<Color/>}></Route>
+              <Route path="whatever" element={<Blog/>}/>
+            </Route>
             <Route path="/fetch" element={<FetchData />}/>
-            <Route path="/blog" element={<Blog />}/>
+            <Route path="/blog">
+              <Route path="" element={<Home/>}/>
+              <Route path="post">
+                <Route path="" element={<Post/>}/>
+                <Route path=":id" element={<PostPage/>}/>
+              </Route>
+            </Route>
             <Route path="*" element={<Navigate to="" replace />} />
         </Route>
 ));
